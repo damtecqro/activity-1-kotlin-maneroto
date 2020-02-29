0296089bc0d397ae300d271e1e71dcec87f7ccd8
maneroto <maneroto@hotmail.com>
2020-02-28 18:56:51 -0600

    Finish Activity

--- TemplateProject/app/src/main/java/com/test/kotlinactivity1/Sandbox.kts
+++ TemplateProject/app/src/main/java/com/test/kotlinactivity1/Sandbox.kts
@@ -1,3 +1,66 @@
 package com.test.kotlinactivity1
 
-println("Hello World")
+import java.lang.Math.sqrt
+
+// Remove an element, problem 20
+
+/*
+* removeElement(array: String, index: int)
+*   from i to array length
+*       if i equals to index
+*       remove array in the ith position
+*   return array
+* finish removeElement
+*
+* It can also be done with the kotlin drop method
+* */
+
+fun removeElement (array: String, index: Int): String
+{
+    var newArray = array.drop(index)
+    return newArray
+}
+
+var removeProblem = readLine()!!.toString()
+var removeIndex = readLine()!!.toInt()
+print("(" + removeElement(removeProblem, removeIndex) + " , " + removeIndex + ")")
+
+
+// Prime factors of a given integer, problem 36
+
+/*
+* primeFactors(number: Int)
+*   while number % 2 different from 0 (it means that it is a pair)
+*       print 2
+*       number = number / 2
+*   nextPrime -> 3
+*   while nextPrime <= root(number)
+*       while number % nextPrime equals 0 (it means that you found another factor)
+*           print nextFactor
+*           number -> number / nextPrime
+*       nextPrime -> nextPrime + 2
+*   if number greater than 2
+*       print number (cus' it is a prime)
+* finish primeFactors
+*
+* */
+
+fun primeFactors(n: Int) {
+    var n = n
+    while (n % 2 == 0) {
+        print("2 ")
+        n /= 2
+    }
+    var i = 3
+    while (i <= sqrt(n.toDouble())) {
+        while (n % i == 0) {
+            print("$i ")
+            n /= i
+        }
+        i += 2
+    }
+    if (n > 2) print("$n ")
+}
+
+var primeProblem = readLine()!!.toInt()
+primeFactors(primeProblem)
